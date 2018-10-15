d35aa7c9c6860ea501df0a8e8f7aa504e232c36d
clifford <clifford@noukod.com>
2018-10-15 05:16:40 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,27 +1,14 @@
-# class Student
-# 
-#     def initialize
-#       @knowledge = []
-#     end
-# 
-#     def learn(string)
-#       @knowledge << string
-#     end
-# 
-#     def knowledge
-#       @knowledge
-#     end
-#   end
+class Student
 
-class Student < User
-   attr_accessor :knowledge
-   def initialize
-    @knowledge = []
-  end
-   def learn(knowledge)
-    @knowledge << knowledge
-  end
-   def knowledge
-    @knowledge
-  end
+    def initialize
+      @knowledge = []
+    end
 
+    def learn(string)
+      @knowledge << string
+    end
+
+    def knowledge
+      @knowledge
+    end
+  end
